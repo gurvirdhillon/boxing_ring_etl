414a410d3425ec7be5f3717702682eb623ec0be1
Gurvir Dhillon <72519948+gurvirdhillon@users.noreply.github.com>
2025-11-27 11:55:37 +0000

    added extract of the photo requests and the extract code

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,7 +1,24 @@
 import pandas as pd
+import os
 
 folder_name = "../../data/raw/"
 file_name = "boxing_matches_messy_data.csv"
+
+
+def check_file_exists(file_path=f"{folder_name}{file_name}") -> bool:
+    if os.path.exists(file_path):
+        print("File path found!")
+        return True
+    else:
+        print("File does not exist")
+        return False
+
+
+def extract_filepath(file_path):
+    if not check_file_exists(file_path):
+        return "file doesnt exist"
+    else:
+        return filepath
 
 
 def extract_data(file_path=f"{folder_name}{file_name}") -> pd.DataFrame:
@@ -16,21 +33,23 @@ def extract_data(file_path=f"{folder_name}{file_name}") -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    df = extract_data()
-    print(df.head())
-    print("-" * 50)
-    print("shape:", df.shape)
-    print("-"* 50)
-    print(df.isnull().sum())
-    print("-"* 50)
-    print("duplicated data:", df.duplicated().sum())
-    print("Data extracted")
+    req = check_file_exists()
+    file
+    if req == True:
+        df = extract_data()
+        print(df.head())
+        print("-" * 50)
+        print("shape:", df.shape)
+        print("-" * 50)
+        print(df.isnull().sum())
+        print("-" * 50)
+        print("duplicated data:", df.duplicated().sum())
+        print("Data extracted")
 
 """
 being in the extract folder and running 
-"python extract.py" 
+"python extract.py"
 will output things such as the first 5 elements
 the shape of the data we are dealing with including 418092 rows and 26 columns
 as well as the number of nulls
 """
-    
---
+++ src/extract/request.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import requests
+
+def get_wikipedia_image(name):
+    url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{name.replace(' ', '_')}"
+    
+    headers = {
+        "User-Agent": "BoxingETL/1.0 (mailto:gurvirsingdhillon@outlook.com)"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print(f"Failed to fetch data for {name}")
+        return None
+
+    data = response.json()
+
+    if "thumbnail" in data:
+        return data["thumbnail"]["source"]
+    else:
+        return None
+
+
+# Example
+fighter_name = "Anthony Joshua"
+print(get_wikipedia_image(fighter_name))
