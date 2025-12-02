91bac29dde1a832ef13408a56268756f2aaeaac3
Gurvir Dhillon <72519948+gurvirdhillon@users.noreply.github.com>
2025-12-02 18:58:02 +0000

    added the presentation stuff

--- src/streamlit/presentation.py
+++ src/streamlit/presentation.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Welcome to the ETL Boxing dataset")
 
-
-
+miro_url = "https://miro.com/app/live-embed/uXjVJh8xNIY=/?embedMode=view_only_without_ui&moveToViewport=-3643%2C-2071%2C6599%2C4931&embedId=252787849467"
+st.components.v1.iframe(miro_url, width=1200, height=800)
